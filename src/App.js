521606969cef0d04d58c4ec606b4f0fe53cb9268
tiziyo.com <tiziyo@hotmail.com>
2024-01-24 16:37:35 +0100

    V1 completed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,105 @@
+import { useState } from "react";
 import "./App.css";
 import Board from "./components/Board";
 
 function App() {
+
+  const [history, setHistory] = useState([{ squares: Array(9).fill(null)}]);
+  const [IsValO, setIsValO] = useState(true);
+  const [stepNumber, setStepNumber] = useState(0);
+
+  const caculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],  
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  };
+
+  const current = history[stepNumber];
+  const winner = caculateWinner(current.squares);
+ 
+  let status;
+  if (winner) {
+    status = `Winner is ${winner} `;
+  } else {
+    status = `Next Player is ${IsValO ? "X" : "O"} `;
+  }
+
+  const handleClick = (i) => {
+
+    //console.log(i);
+    console.log("stepNumber:", stepNumber);
+    const history_copy = history.slice(0, stepNumber + 1 );
+    const new_current = history_copy[stepNumber];
+    const squares_copy = new_current.squares.slice();
+
+    if ( caculateWinner(squares_copy) || squares_copy[i]) {
+      return;
+    }
+
+    squares_copy[i] = IsValO ? "X" : "O";
+    setHistory([...history_copy, {squares: squares_copy}]);
+    // setIsValO(!IsValO);
+    setIsValO(prev => !prev);
+
+    setStepNumber(stepNumber + 1);
+  }; 
+
+  const moves = history.map((step, move) => {
+
+    console.log("step:", step);
+    console.log("move:", move);
+
+    const desc = move ? 
+      'Go to move #' + move :
+      'Go to game start' ;
+    return (
+      <li key={move}>
+        <button className="move-button" onClick={() => jumpTo(move)}> {desc} </button>
+      </li>
+    );
+  });
+
+  const jumpTo = (step) => {
+    setStepNumber(step);
+    setIsValO((step % 2) === 0);
+  };
+
   return (
-    <div className="game">
+    <div className="game"> 
+      <div className="title">TIC TAC TOE</div>
       <div className="game-board">
-        <Board xxx="JJJ" />
+        <Board 
+          squares={current.squares}
+          onClick={(i) => handleClick(i)}
+        />
       </div>
-      <div className="game-info">Game Info</div>
+      <div className="game-info">
+        <div className="status"> {status} </div>
+        <ol style={{listStyle: 'none'}}>
+          {moves}
+        </ol>
+      </div>
     </div>
   );
 }
-
+  
 export default App;
--- src/components/Board.js
+++ src/components/Board.js
@@ -2,74 +2,25 @@ import React, { useState } from "react";
 import Square from "./Square";
 import "./Board.css";
 
-const Board = () => {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [IsValO, setIsValO] = useState(true);
-  let status;
-
-  const caculateWinner = () => {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (
-        squares[a] &&
-        squares[a] === squares[b] &&
-        squares[a] === squares[c]
-      ) {
-        return squares[a];
-      }
-    }
-    return null;
-  };
-
-  let winner = caculateWinner();
-  if (winner) {
-    status = `Winner is  ${winner} `;
-  } else {
-    status = `Next Player is  ${IsValO ? "X" : "O"} `;
-  }
-
-
-  const handleClick = (i) => {
-    const squares_copy = squares.slice();
-    if (winner !== null || squares[i] !== null) {
-      return;
-    }
-
-    squares_copy[i] = IsValO ? "X" : "O";
-    setSquares(squares_copy);
-    // setIsValO(!IsValO);
-    setIsValO((IsValO) => !IsValO);
-  };
+const Board = ({squares, onClick}) => {
 
   const renderSquare = (i) => {
-    return <Square value={squares[i]} onClick={() => handleClick(i)} />;
+    return <Square value={squares[i]} onClick={() => onClick(i)} />;
   };
 
   return (
-    <div>
-      <div className="status"> ${status} </div>
-      <div className="board-row">
+    <div className='board-wrapper'>
+      <div className='board-row'>
         {renderSquare(0)}
         {renderSquare(1)}
         {renderSquare(2)}
       </div>
-      <div className="board-row">
+      <div className='board-row'>
         {renderSquare(3)}
         {renderSquare(4)}
         {renderSquare(5)}
       </div>
-      <div className="board-row">
+      <div className='board-row'>
         {renderSquare(6)}
         {renderSquare(7)}
         {renderSquare(8)}
